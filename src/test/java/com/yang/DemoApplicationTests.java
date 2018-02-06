bfe842c7f023f948fd9a8e8baa44983d66c7c455
leavesyang <yangtianxi2011@126.com>
2018-02-06 13:18:38 +0800

    Spring Boot中的缓存支持（一）注解配置与EhCache使用

--- src/test/java/com/yang/DemoApplicationTests.java
+++ src/test/java/com/yang/DemoApplicationTests.java
@@ -1,49 +1,40 @@
 package com.yang;
 
+import com.yang.domain.User;
+import com.yang.domain.UserRepository;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.cache.CacheManager;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = {DemoApplication.class})
+@SpringApplicationConfiguration(DemoApplication.class)
 public class DemoApplicationTests {
 
     @Autowired
-    @Qualifier("primaryJdbcTemplate")
-    protected JdbcTemplate jdbcTemplate1;
+    private CacheManager cacheManager;
 
     @Autowired
-    @Qualifier("secondaryJdbcTemplate")
-    protected JdbcTemplate jdbcTemplate2;
+    private UserRepository userRepository;
 
     @Before
-    public void setUp() {
-        jdbcTemplate1.update("DELETE  FROM  USER ");
-        jdbcTemplate2.update("DELETE  FROM  USER ");
+    public void before() {
+        userRepository.save(new User("AAA", 10));
     }
 
     @Test
     public void test() throws Exception {
+        User u1 = userRepository.findByName("AAA");
+        System.out.println("第一次查询：" + u1.getAge());
 
-        // 往第一个数据源中插入两条数据
-        jdbcTemplate1.update("insert into user(id,name,age) values(?, ?, ?)", 1, "aaa", 20);
-        jdbcTemplate1.update("insert into user(id,name,age) values(?, ?, ?)", 2, "bbb", 30);
-
-        // 往第二个数据源中插入一条数据，若插入的是第一个数据源，则会主键冲突报错
-        jdbcTemplate2.update("insert into user(id,name,age) values(?, ?, ?)", 1, "aaa", 20);
-
-        // 查一下第一个数据源中是否有两条数据，验证插入是否成功
-        Assert.assertEquals("2", jdbcTemplate1.queryForObject("select count(1) from user", String.class));
-
-        // 查一下第一个数据源中是否有两条数据，验证插入是否成功
-        Assert.assertEquals("1", jdbcTemplate2.queryForObject("select count(1) from user", String.class));
-
+        User u2 = userRepository.findByName("AAA");
+        System.out.println("第二次查询：" + u2.getAge());
     }
 
 
